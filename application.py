cdfa4fe7b6b26bc1531c0d36173eb9c70871d06b
s3632442 <s363244@student.rmit.edu.au>
2023-10-19 17:33:28 +1100

    loading images

--- application.py
+++ application.py
@@ -7,6 +7,8 @@ import boto3
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'ac3b06a1-6db9-4e2a-b74a-ea24572ed710'
 
+# Initialize the DynamoDB resource and specify the region
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 @app.route("/")
 def home():
@@ -138,9 +140,6 @@ def insert_initial_logins(dynamodb=None):
         
         table.put_item(Item=item)
 
-# Initialize the DynamoDB resource and specify the region
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-
 # Define the table name and attributes
 table_name = 'music'
 table_attributes = [
@@ -218,7 +217,93 @@ def load_data_to_table():
     except Exception as e:
         print(f'Error loading data: {e}')
 
+from flask import Flask, redirect, request, render_template, flash
+
+import json
+import os
+import requests
+import boto3
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'ac3b06a1-6db9-4e2a-b74a-ea24572ed710'
+
+# ... (Your existing code)
+
+# Path to your a2.json file
+json_file_path = 'a2.json'
+
+# ... (Your existing code)
+
+def image_exists_in_s3(s3, bucket_name, s3_object_key):
+    try:
+        # Attempt to head the S3 object (check for existence)
+        s3.head_object(Bucket=bucket_name, Key=s3_object_key)
+        return True
+    except Exception as e:
+        # An exception is raised if the object doesn't exist
+        return False
+
+def download_and_upload_images():
+    # Load the JSON data
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    # Define the S3 bucket name
+    bucket_name = '201c4962-cb1a-4775-9b92-889393597be0'
+
+
+    s3 = boto3.client('s3', region_name='us-east-1')  # Replace 'us-east-1' with your desired region
+
+    # Check if the S3 bucket exists, and create it if not
+    if bucket_name not in [bucket['Name'] for bucket in s3.list_buckets()['Buckets']]:
+        s3.create_bucket(Bucket=bucket_name)
+
+    # Directory to store downloaded images temporarily
+    download_dir = 'downloaded_images'
+
+    # Create the directory if it doesn't exist
+    os.makedirs(download_dir, exist_ok=True)
+
+    # Loop through each song and upload the image to S3
+    for song in data['songs']:
+        image_url = song['img_url']
+        artist_name = song['artist']
+
+        # Generate a unique key for the S3 object using the artist's name
+        s3_object_key = f'artists/{artist_name}.jpg'
+
+        if image_exists_in_s3(s3, bucket_name, s3_object_key):
+            print(f'Image for {artist_name} already exists in S3. Skipping.')
+            continue
+
+        # Download the image
+        response = requests.get(image_url)
+
+        if response.status_code == 200:
+            # Save the downloaded image to the local directory
+            image_path = os.path.join(download_dir, f'{artist_name}.jpg')
+            with open(image_path, 'wb') as image_file:
+                image_file.write(response.content)
+
+            # Upload the image to S3
+            s3.upload_file(image_path, bucket_name, s3_object_key)
+
+            # Clean up: remove the local image file
+            os.remove(image_path)
+
+            print(f'Uploaded image for {artist_name} to S3.')
+
+        else:
+            print(f'Failed to download the image for {artist_name}.')
+
+    print('Image upload to S3 completed.')
+
+# ... (Your existing code)
+
 if __name__ == '__main__':
     create_music_table()  # Create the DynamoDB table if it doesn't exist
     load_data_to_table()  # Load data from a2.json into the table if it's empty
+
+    download_and_upload_images()  # Download and upload artist images to S3
+
     app.run(host='0.0.0.0')
