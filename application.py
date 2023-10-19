0512001f800350a3ca80494d5cb70d23a51799f4
s3632442 <s363244@student.rmit.edu.au>
2023-10-19 14:31:20 +1100

    loginbox

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, request, render_template
+
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def home():
+    return redirect("/login")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Add code here to handle the login form submission
+        # You can check the username and password and redirect to another page on successful login.
+        return "Login successful, redirect to another page."
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
